563a42554d82d8d9e133945f524bdf0ee54f3f5f
juancece <juancece@outlook.com>
2020-07-16 20:59:03 -0500

    Ep6

--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -5,91 +5,19 @@
       <div v-for="t in tricks">
         {{ t.name}}
         <div>
-          <video width="400" controls :src="`http://localhost:5000/api/videos/${t.video}`"></video>
+          <video :src="`http://localhost:5000/api/videos/${t.video}`" controls width="400"></video>
         </div>
       </div>
     </div>
 
 
-    <v-stepper v-model="step">
-
-      <v-stepper-header>
-        <v-stepper-step :complete="step > 1" step="1">Upload Video</v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step :complete="step > 2" step="2">Trick Information</v-stepper-step>
-
-        <v-divider></v-divider>
-
-        <v-stepper-step step="3">Confirmation</v-stepper-step>
-      </v-stepper-header>
-
-      <v-stepper-items>
-        <v-stepper-content step="1">
-          <div>
-            <v-file-input @change="handleFile" accept="video/*"></v-file-input>
-          </div>
-        </v-stepper-content>
-
-        <v-stepper-content step="2">
-          <div>
-            <v-text-field label="Tricking Name" v-model="trickName"></v-text-field>
-            <v-btn @click="saveTrick">Save Trick</v-btn>
-          </div>
-
-        </v-stepper-content>
-
-        <v-stepper-content step="3">
-          <div>
-            Success
-          </div>
-        </v-stepper-content>
-      </v-stepper-items>
-
-    </v-stepper>
-
   </div>
 </template>
 
 <script>
-  import {mapState, mapActions, mapMutations} from 'vuex';
+  import {mapState} from 'vuex';
 
   export default {
-    data: () => ({
-      trickName: "",
-      step: 1
-    }),
-    computed: {
-      ...mapState('tricks', ['tricks']),
-      ...mapState('videos', ['uploadPromise']),
-    },
-    methods: {
-      ...mapMutations('videos', {
-        resetVideos: 'reset'
-      }),
-      ...mapActions('tricks', ['createTrick']),
-      ...mapActions('videos', ['startVideoUpload']),
-      async handleFile(file) {
-        if (!file) return;
-
-        const form = new FormData();
-        form.append("video", file);
-        this.startVideoUpload({form});
-        this.step++;
-      },
-      async saveTrick() {
-        if (!this.uploadPromise) {
-          console.log("uploadTask is null")
-          return;
-        }
-
-        const video = await this.uploadPromise;
-        await this.createTrick({trick: {name: this.trickName, video}});
-        this.trickName = ""
-        this.step++
-        this.resetVideos();
-      }
-    }
+    computed: mapState('tricks', ['tricks']),
   }
 </script>
--- web-client/store/tricks.js
+++ web-client/store/tricks.js
@@ -17,9 +17,5 @@ export const actions = {
   async fetchTricks({commit}) {
     const tricks = await this.$axios.$get("http://localhost:5000/api/tricks");
     commit("setTrick", {tricks})
-  },
-  async createTrick({commit, dispatch}, {trick}) {
-    await this.$axios.$post("http://localhost:5000/api/tricks", trick);
-    await dispatch('fetchTricks')
   }
 }
